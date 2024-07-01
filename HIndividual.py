508a2a36946b9eebab8b3d4c5a3313fffa6d1ea3
satyajeetrai007 <126376637+satyajeetrai007@users.noreply.github.com>
2024-07-01 19:37:55 +0530

    Update HIndividual.py
    modified hinglish.txt file path .

--- HIndividual.py
+++ HIndividual.py
@@ -169,7 +169,7 @@ def iword_cloud(df,user):
     
 def imost_common_words(df,user):
     df = df[df['users'] == user]
-    f = open('.venv\stop_hinglish.txt','r', encoding='utf-8')
+    f = open('stop_hinglish.txt','r', encoding='utf-8')
     stop_words = f.read().split()
 
     temp = df[df['users'] != 'group_notification']
